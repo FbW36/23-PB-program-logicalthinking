73f76ed6f9a19bf3033a062e08b1e3e4403349db
Zarzarius <zarzarius@gmail.com>
2020-10-26 15:43:46 +0100

    exercise done

---
+++ Azael/index.js
@@ -0,0 +1,3 @@
+// 1. Log in the value of the biggest number of two given numbers.
+// 2. Compare the value of two given numbers and if they are equal print true.
+// 3. Check if the value of a number is bigger than 100.
