ed0c0ec2380ab6e9401725d080b2fc8f04be39a9
Tomas <eliz.tomas23@gmail.com>
2020-10-26 15:31:37 +0100

    Tomas' suggestions for #23

---
+++ tomas/index.js
@@ -0,0 +1,5 @@
+// 1. return the bigger value of two integers
+
+// 2. compare two integers and return corresponding boolean value
+
+// 3. if two given integers are greater than or equal 100, return true, otherwise return false
