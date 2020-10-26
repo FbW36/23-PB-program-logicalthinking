bb0c085e243b6da305c203ae8a07601e3fe3c205
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-10-26 13:49:01 +0100

    First commit

---
+++ adrian/script.js
@@ -0,0 +1,14 @@
+//1
+/* Given two numbers return the biggest one
+ */
+
+//2
+/*
+ Compare two numbers and return true if they are equal or false if they are not. 
+  */
+
+//3
+
+/*
+  Given two numbers a and b, return true if a - b is greater than 0, and false if a - b is smaller than 0, 
+   */
