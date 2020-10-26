87c1221fe9236205fd60a38d43fa50dbb26cd008
Chrissi <cmette.online@googlemail.com>
2020-10-26 15:25:49 +0100

    Add solutions

---
+++ christin/index.js
@@ -0,0 +1,24 @@
+// ToDO Write down pseudo-code on pen and paper for the following patterns:
+/*Input	Output
+2, 5	5
+0, 100	100
+52, 35	52*/
+
+//! Create two values and if one is higher than the other, print the higher one.
+
+// TODO
+/* 2. Input	Output
+5, 5	true
+40, 40	true
+50, 51	false*/
+
+//!If the integers are equal, print true.
+
+// ToDO
+/* 3.
+Input	Output
+110, 100	true
+150, 100	true
+50, 100      false*/
+
+//! create two integers , if the both are  bigger than 100, print true .
