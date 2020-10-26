a9864234a6108e2b3c722aebc706ba77d1f7de3c
carveler <3heyaberlin@gmail.com>
2020-10-26 15:43:50 +0100

    mami

---
+++ mami/script.js
@@ -0,0 +1,43 @@
+let a = 2;
+let b= 5;
+let c =0;
+let d = 100;
+let e = 52;
+let f = 35;
+
+console.log(b>a ? b: a)
+console.log(d>c ? d: c)
+console.log(e>f ? e: f)
+
+
+let g = 40
+let h = 50
+let j = 51
+console.log(b === b ? true:false);
+if(g === g) {
+    console.log(true)
+}else console.log(false)
+
+if(g === g) {
+    console.log(true)
+}else console.log(false)
+
+if(h === j) {
+    console.log(true)
+}else console.log(false)
+
+
+let k = 110
+let l = 100
+let m =150
+if(k>l) {
+    console.log(true)
+}else console.log(false)
+
+if(m>l) {
+    console.log(true)
+}else console.log(false)
+
+if(h>l) {
+    console.log(true)
+}else console.log(false)
