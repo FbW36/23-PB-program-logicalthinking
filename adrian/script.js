d73fcb25805214950e2ee6b0b2050792453b5f8e
AdrianValdes <adrianvaldeshdz@gmail.com>
2020-10-27 14:54:57 +0100

    First commit

--- adrian/script.js
+++ adrian/script.js
@@ -1,14 +1,21 @@
 //1
 /* Given two numbers return the biggest one
  */
-
+let bigNumber = 5;
+let smallNumber = 2;
+Math.max(bigNumber, smallNumber);
 //2
 /*
  Compare two numbers and return true if they are equal or false if they are not. 
   */
-
+let number = 5;
+let number1 = 5;
+number === number1 ? true : false;
 //3
 
 /*
   Given two numbers a and b, return true if a - b is greater than 0, and false if a - b is smaller than 0, 
    */
+let number2 = 110;
+let number100 = 100;
+number2 > 100 ? true : false;
